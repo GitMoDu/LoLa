7bbfff07169d61070c6a45cc893b127c65c98627
André <batatas@gmail.com>
2018-05-25 14:54:49 +0100

    Added example remote project.

---
+++ examples/ExampleRemote/RemoteManager.h
@@ -0,0 +1,49 @@
+// LoLaManagerRCReceiver.h
+
+#ifndef _LOLAMANAGERRCRECEIVER_h
+#define _LOLAMANAGERRCRECEIVER_h
+
+#include <LoLaManager.h>
+#include <Services\SyncSurface\CommandInput\CommandInputSurfaceReader.h>
+#include <Services\SyncSurface\Telemetry\TelemetrySurfaceWriter.h>
+
+class LoLaManagerRCReceiver : public LoLaManagerRemote
+{
+protected:
+	CommandInputSurfaceReader* CommandInput;
+	TelemetrySurfaceWriter* Telemetry;
+
+protected:
+	virtual bool OnSetupServices()
+	{
+		if (!LoLa->GetServices()->Add(CommandInput))
+		{
+			return false;
+		}
+
+		if (!LoLa->GetServices()->Add(Telemetry))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+public:
+	LoLaManagerRCReceiver(Scheduler* scheduler, LoLaPacketDriver* loLa)
+		: LoLaManagerRemote(scheduler, loLa)
+	{
+		CommandInput = new CommandInputSurfaceReader(scheduler, loLa);
+		Telemetry = new TelemetrySurfaceWriter(scheduler, loLa);
+	}
+
+
+	CommandInputSurface* GetCommandInputSurface()
+	{
+		return (CommandInputSurface*)CommandInput->GetSurface();//TODO: Make sure this cast does not cause problems.
+	}
+};
+
+
+#endif
+
