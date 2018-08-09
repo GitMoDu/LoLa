e100070982afbbbea470591c152441d9eb9c9b07
GitMoDu <batatas@gmail.com>
2018-08-09 15:30:15 +0100

    Added simple clock tracker.

---
+++ src/ClockSource.h
@@ -0,0 +1,57 @@
+// ClockSource.h
+
+#ifndef _CLOCKSOURCE_h
+#define _CLOCKSOURCE_h
+
+#include <stdint.h>
+
+class ClockSource
+{
+private:
+	uint32_t Offset = 0;
+	uint32_t LastSetMillis = 0;
+
+public:
+	ClockSource()
+	{
+		Reset();
+	}
+
+	void Reset()
+	{
+		Offset = 0;
+		LastSetMillis = 0;
+	}
+
+	void SetMillis(const uint32_t timeMillis)
+	{
+		LastSetMillis = millis();
+		Offset = timeMillis - LastSetMillis;
+	}
+
+	void AddOffset(const int16_t offset)
+	{
+		Offset += offset;
+	}
+
+	uint32_t GetMillis()
+	{
+		return millis() + Offset;
+	}
+
+	uint32_t GetMicros()
+	{
+		return GetMillis()*1000L + micros();
+	}
+
+	uint32_t GetLastSyncElapsedMillis()
+	{
+		if (LastSetMillis != 0)
+		{
+			return millis() - LastSetMillis;
+		}
+		return UINT32_MAX;
+	}
+};
+
+#endif
