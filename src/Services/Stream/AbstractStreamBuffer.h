99e88ee1ab1a594e094b4d6985a4c3e1fd1fb30a
GitMoDu <batatas@gmail.com>
2018-10-12 16:31:49 +0100

    Use variable, not enum.

--- src/Services/Stream/AbstractStreamBuffer.h
+++ src/Services/Stream/AbstractStreamBuffer.h
@@ -93,7 +93,7 @@ protected:
 
 	void OnService()
 	{
-		switch (StreamStateEnum)
+		switch (StreamState)
 		{
 		case StreamStateEnum::WaitingForServiceDiscovery:
 			OnWaitingForServiceDiscovery();
