ea1f21d08d9959c091d7e071cacf91b7c923d7c4
GitMoDu <batatas@gmail.com>
2019-05-14 15:32:12 +0100

    Updated statistics.

--- src/LoLaClock/ClockErrorStatistics.h
+++ src/LoLaClock/ClockErrorStatistics.h
@@ -15,10 +15,9 @@ public:
 
 public:
 	ClockErrorSample() {}
-	ClockErrorSample(const int32_t errorMicros, const uint32_t periodMillis)
+	void Set(const int32_t absoluteErrorMicros, const uint32_t periodMillis)
 	{
-		ErrorMicros = errorMicros;
-		PeriodMillis = periodMillis;
+		ErrorMicros = (int32_t)(((int64_t)absoluteErrorMicros) / ((int64_t)(periodMillis / 1000)));
 	}
 };
 
@@ -28,6 +27,7 @@ class ClockErrorStatistics
 public:
 	ClockErrorSample Grunt;
 
+	static const size_t MIN_ELEMENTS = min(LOLA_LINK_SERVICE_LINKED_CLOCK_MIN_SAMPLES, ERROR_SAMPLE_COUNT);
 	static const uint32_t MAX_CLOCK_ERROR_MICROS = LOLA_LINK_SERVICE_LINKED_CLOCK_OK_ERROR_MICROS;
 
 	RingBufCPP<ClockErrorSample, ERROR_SAMPLE_COUNT> TuneErrorSamples;
@@ -36,11 +36,11 @@ public:
 
 	//Statistics values.
 	int32_t AverageErrorMicros = 0;
-	int64_t AverageWeightedErrorMicros = 0;
-	uint32_t WeightedTotalPeriodMillis = 0;
 	uint32_t MaxErrorMicros = 0;
 
 	uint8_t Count = 0;
+
+
 
 public:
 	void AddTuneSample(const int32_t errorMicros, const uint32_t periodMillis)
@@ -54,7 +54,12 @@ public:
 
 	bool HasEnoughSamples()
 	{
-		return TuneErrorSamples.numElements() > (ERROR_SAMPLE_COUNT / 2);
+		return TuneErrorSamples.numElements() > MIN_ELEMENTS;
+	}
+
+	bool HasFullSamples()
+	{
+		return TuneErrorSamples.isFull();
 	}
 
 	void AddTuneSample(const ClockErrorSample errorSample)
@@ -75,6 +80,11 @@ public:
 			map(min(MaxErrorMicros, MAX_CLOCK_ERROR_MICROS), MAX_CLOCK_ERROR_MICROS, 0, 0, UINT8_MAX)) / 3;
 	}
 
+	uint32_t GetMaxErrorMicros()
+	{
+		return MaxErrorMicros;
+	}
+
 	int32_t GetAverageError()
 	{
 		if (NeedsUpdate)
@@ -83,26 +93,6 @@ public:
 		}
 
 		return AverageErrorMicros;
-	}
-
-	int32_t GetWeightedAverageError()
-	{
-		if (NeedsUpdate)
-		{
-			UpdateStatistics();
-		}
-
-		return AverageWeightedErrorMicros;
-	}
-
-	uint32_t GetWeightedAverageDurationMillis()
-	{
-		if (NeedsUpdate)
-		{
-			UpdateStatistics();
-		}
-
-		return WeightedTotalPeriodMillis;
 	}
 
 	void Reset()
@@ -123,15 +113,11 @@ private:
 			Count = 0;
 			MaxErrorMicros = 0;
 			AverageErrorMicros = 0;
-			AverageWeightedErrorMicros = 0;
-			WeightedTotalPeriodMillis = 0;
+			
 			while (Count < TuneErrorSamples.numElements())
 			{
 				Grunt = *TuneErrorSamples.peek(Count);
 				AverageErrorMicros += Grunt.ErrorMicros;
-				AverageWeightedErrorMicros += (int64_t)Grunt.ErrorMicros * (int64_t)(Grunt.PeriodMillis);
-				WeightedTotalPeriodMillis += Grunt.PeriodMillis;
-
 				if (abs(Grunt.ErrorMicros) > MaxErrorMicros)
 				{
 					MaxErrorMicros = abs(Grunt.ErrorMicros);
@@ -140,7 +126,6 @@ private:
 			}
 
 			AverageErrorMicros /= Count;
-			AverageWeightedErrorMicros = (int32_t)((int64_t)AverageWeightedErrorMicros / (int64_t)WeightedTotalPeriodMillis);
 
 			NeedsUpdate = false;
 		}
