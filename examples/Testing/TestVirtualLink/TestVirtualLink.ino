bad061759b6d47aa9fead5e0151efc167b909d7b
GitMoDu <batatas@gmail.com>
2023-07-31 20:37:49 +0100

    Update simulation example.

--- examples/Testing/TestVirtualLink/TestVirtualLink.ino
+++ examples/Testing/TestVirtualLink/TestVirtualLink.ino
@@ -95,16 +95,17 @@ static const uint8_t ClientPrivateKey[LoLaCryptoDefinition::PRIVATE_KEY_SIZE] = 
 // Virtual Transceiver configurations.
 // <ChannelCount, TxBaseMicros, TxByteNanos, AirBaseMicros, AirByteNanos, HopMicros>
 using SlowSingleChannel = IVirtualTransceiver::Configuration<1, 100, 200, 500, 4000, 50>;
-using FastSingleChannel = IVirtualTransceiver::Configuration<1, 20, 50, 100, 1000, 50>;
-using FastMultiChannel = IVirtualTransceiver::Configuration<20, 20, 50, 50, 1000, 50>;
+using FastSingleChannel = IVirtualTransceiver::Configuration<1, 20, 50, 100, 1000, 20>;
+using SlowMultiChannel = IVirtualTransceiver::Configuration<10, 100, 200, 500, 4000, 50>;
+using FastMultiChannel = IVirtualTransceiver::Configuration<50, 20, 50, 50, 1000, 20>;
 
 // Used Virtual Driver Configuration.
 using TestRadioConfig = SlowSingleChannel;
 
 // Shared Link configuration.
-static const uint16_t DuplexPeriod = 4000;
-static const uint16_t DuplexDeadZone = 100;
-static const uint32_t ChannelHopPeriod = 20000;
+static const uint16_t DuplexPeriod = 2000;
+static const uint16_t DuplexDeadZone = FastMultiChannel::HopMicros;
+static const uint32_t ChannelHopPeriod = DuplexPeriod * 2;
 
 // Use best available sources.
 #if defined(ARDUINO_ARCH_STM32F1) || defined(ARDUINO_ARCH_STM32F4)
