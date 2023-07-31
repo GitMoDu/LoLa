1425c99a97ed9ce6d84e149fb448b719a233ca09
GitMoDu <batatas@gmail.com>
2023-07-31 20:36:52 +0100

    Tuned constants.

--- src/LoLaLinks/Abstract/AbstractLoLaLinkClient.h
+++ src/LoLaLinks/Abstract/AbstractLoLaLinkClient.h
@@ -19,8 +19,8 @@ private:
 	static constexpr uint32_t CLIENT_SLEEP_TIMEOUT_MILLIS = 30000;
 	static constexpr uint32_t CHANNEL_SEARCH_TRY_COUNT = 3;
 
-	static constexpr uint32_t CLIENT_AUTH_REQUEST_WAIT_TIMEOUT_MILLIS = 50;
-	static constexpr uint8_t CLOCK_TUNE_FILTER_RATIO = 110;
+	static constexpr uint32_t CLIENT_AUTH_REQUEST_WAIT_TIMEOUT_MILLIS = 100;
+	static constexpr uint8_t CLOCK_TUNE_FILTER_RATIO = 130;
 
 	enum WaitingStateEnum
 	{
