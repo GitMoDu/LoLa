d8a96de1e9fca8f4d2c7591286869aa03cedde11
GitMoDu <batatas@gmail.com>
2024-02-19 20:19:18 +0000

    Clean-up include file.

--- src/ILoLaInclude.h
+++ src/ILoLaInclude.h
@@ -8,25 +8,6 @@
 #include "Link\ILoLaLink.h"
 #include "Link\LoLaLinkDefinition.h"
 
-//TODO: Refactor as individual driver includes
-/// Available Packet Drivers
-#include "LoLaTransceivers\VirtualTransceiver\VirtualTransceiver.h"
-#include "LoLaTransceivers\UartTransceiver\UartTransceiver.h"
-#include "LoLaTransceivers\nRF24Transceiver\nRF24Transceiver.h"
-#include "LoLaTransceivers\Si446xTransceiver\Si446xTransceiver.h"
-#include "LoLaTransceivers\Si446xTransceiver2\Si446xTransceiver2.h"
-#include "LoLaTransceivers\Sx12Transceiver\Sx12Transceiver.h"
-#if defined(ARDUINO_ARCH_ESP8266)
-#include "LoLaTransceivers\EspNowTransceiver\Esp8266NowTransceiver.h"
-#elif defined(ARDUINO_ARCH_ESP32)
-#include "LoLaTransceivers\EspNowTransceiver\Esp32NowTransceiver.h"
-#endif
-#if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_STM32F1)
-#include "LoLaTransceivers\PimTransceiver\PimTransceiver.h"
-#endif
-///
-
-
 /// Entropy Sources
 #if defined(ARDUINO)
 #include "EntropySources\ArduinoEntropy.h"
@@ -45,6 +26,14 @@
 #endif 
 ///
 
+/// Duplex Options
+#include "Duplexes\Duplexes.h"
+///
+
+/// Channel Hopper Options
+#include "ChannelHoppers\FixedHoppers.h"
+#include "ChannelHoppers\TimedHoppers.h"
+///
 
 /// Clock Timer Sources
 #if defined(ARDUINO)
@@ -64,30 +53,40 @@
 
 
 /// Available Link Modules
-#include "LoLaLinks\LolaPkeLink\LoLaPkeLinkServer.h"
-#include "LoLaLinks\LolaPkeLink\LoLaPkeLinkClient.h"
-
 #include "LoLaLinks\LolaAddressMatchLink\LoLaAddressMatchLinkServer.h"
 #include "LoLaLinks\LolaAddressMatchLink\LoLaAddressMatchLinkClient.h"
 
+#include "LoLaLinks\LolaPkeLink\LoLaPkeLinkServer.h"
+#include "LoLaLinks\LolaPkeLink\LoLaPkeLinkClient.h"
+///
+
 /// Available Link Based Services
-#if defined(LOLA_DEBUG)
-//#include "Services\Debug\DiscoveryTestService.h"
-#endif
+#include "Service\TemplateLoLaService.h"
+
+#include "LoLaServices\AbstractLoLaDiscoveryService.h"
+
 #include "LoLaServices\SyncSurface\SyncSurfaceReader.h"
 #include "LoLaServices\SyncSurface\SyncSurfaceWriter.h"
+
 //#include "Services\BinaryDelivery\BinaryDeliveryService.h"
 //#include "Services\Stream\ForwardCorrectedLossyStream.h"
 ///
 
-/// Duplex Options
-#include "Duplexes\Duplexes.h"
-///
 
-
-/// Channel Hopper Options
-#include "ChannelHoppers\FixedHoppers.h"
-#include "ChannelHoppers\TimedHoppers.h"
+/// Available Transceiver Drivers
+#include "LoLaTransceivers\VirtualTransceiver\VirtualTransceiver.h"
+#include "LoLaTransceivers\UartTransceiver\UartTransceiver.h"
+#include "LoLaTransceivers\nRF24Transceiver\nRF24Transceiver.h"
+#include "LoLaTransceivers\Si446xTransceiver\Si446xTransceiver.h"
+#include "LoLaTransceivers\Sx12Transceiver\Sx12Transceiver.h"
+#if defined(ARDUINO_ARCH_ESP8266)
+#include "LoLaTransceivers\EspNowTransceiver\Esp8266NowTransceiver.h"
+#elif defined(ARDUINO_ARCH_ESP32)
+#include "LoLaTransceivers\EspNowTransceiver\Esp32NowTransceiver.h"
+#endif
+#if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_STM32F1)
+#include "LoLaTransceivers\PimTransceiver\PimTransceiver.h"
+#endif
 ///
 
 #endif
